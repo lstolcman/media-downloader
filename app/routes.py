89e0836902d1919d3d003c7a83ba5eb77a9543c0
Lukasz <lukasz@kkthx.pl>
2018-02-08 01:41:39 +0100

    framework for youtube download

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,94 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-@app.route('/')
-@app.route('/index')
+import youtube_dl
+
+
+def humanize_bytes(bytes, precision=1):
+    """Return a humanized string representation of a number of bytes.
+
+    Assumes `from __future__ import division`.
+
+    >>> humanize_bytes(1)
+    '1 byte'
+    >>> humanize_bytes(1024)
+    '1.0 kB'
+    >>> humanize_bytes(1024*123)
+    '123.0 kB'
+    >>> humanize_bytes(1024*12342)
+    '12.1 MB'
+    >>> humanize_bytes(1024*12342,2)
+    '12.05 MB'
+    >>> humanize_bytes(1024*1234,2)
+    '1.21 MB'
+    >>> humanize_bytes(1024*1234*1111,2)
+    '1.31 GB'
+    >>> humanize_bytes(1024*1234*1111,1)
+    '1.3 GB'
+    """
+    abbrevs = (
+        (1<<50, 'PB'),
+        (1<<40, 'TB'),
+        (1<<30, 'GB'),
+        (1<<20, 'MB'),
+        (1<<10, 'kB'),
+        (1, 'bytes')
+    )
+    if bytes == 1:
+        return '1 byte'
+    for factor, suffix in abbrevs:
+        if bytes >= factor:
+            break
+    return '%.*f %s' % (precision, bytes / factor, suffix)
+
+def humanize_time(seconds):
+    """Return a humanized string representation of a number of seconds"""
+    abbrevs = (
+        (60*60*24, 'd'),
+        (60*60, 'h'),
+        (60, 'm'),
+        (1, 's')
+    )
+    if seconds == 1:
+        return '1s'
+    s = ''
+    for factor, suffix in abbrevs:
+        if seconds >= factor:
+            tmp = seconds
+            s += str(int(seconds/factor)) + suffix
+            seconds -= factor*int(seconds/factor)
+    return s
+
+
+class InputURL(FlaskForm):
+    url = StringField('Video URL', validators=[DataRequired()])
+    submit = SubmitField('Get')
+
+@app.route('/', methods=['GET', 'POST'])
+#@app.route('/index', methods=['GET', 'POST'])
 def index():
-    users = {'username' : 'habibi'}
-    posts = [
-                {
-                    'author' : {'username' : 'John'},
-                    'body' : 'Test post by john'
-                },
-                {
-                    'author' : {'username' : 'Mary'},
-                    'body' : 'The avngers movie'
-                }
-            ]
-    return render_template('index.html', title='Home', user=users, posts=posts)
+    form = InputURL()
+    if form.validate_on_submit():
+        #return redirect(url_for('dl'))
+        ydl_opts = {'listsubtitles':True}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            test_url = 'http://www.youtube.com/watch?v=BaW_jenozKc'
+            info = ydl.extract_info(form.url.data, download=False)
 
+        return dl(info)
+    return render_template('index.html', form=form, general=None)
+
+@app.route('/dl', methods=['GET', 'POST'])
+def dl(info=None):
+    if not info:
+        return redirect(url_for('index'))
+
+    if 'duration' in info:
+        info['duration'] = humanize_time(info['duration'])
+    for f in info['formats']:
+        if 'filesize' in f:
+            f['filesize'] = humanize_bytes(f['filesize'])
+    return render_template('dl.html', info=info)
